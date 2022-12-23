388681616c06263b824b9c15147117655beb6a4f
tinamai <tinatlmai@hotmail.com>
2022-12-23 13:04:15 -0800

    FINAL WORKING VERSION :)))
    DIRECTIONS:
    - Run TypingTest.java
    - In the terminal, type in the artist/singer you want to type to. We'll find their most popular song (according to Genius) and display 5 words at a time on the screen
    - Once the song has loaded, go to the processing window and press ENTER to start (timer will start once you press ENTER)
    - Type until the timer reaches one minute (you'll be stopped once time runs out)
    - Note: this is case-sensitive, spaces and punctuation matter, and you'll need to type a SPACE at the end of a line to move on
    - If you make a mistake, check the terminal to see where you're at (don't worry about deleting your mistakes)
    
    WARNING: There's no validation...
    We recommend "Taylor Swift" for testing :)

--- src/main/java/TypingTest.java
+++ src/main/java/TypingTest.java
@@ -3,22 +3,20 @@ import processing.core.PApplet;
 public class TypingTest extends PApplet {
 
     Scanner s;
-    ArrayList<String> words;
-    int wordIndex;
-    int letterIndex;
-    int k;
+
+    ArrayList<String> words; // ArrayList of each word in the song
+    int wordIndex; // the index of thewords on the screen (0-4)
+    int letterIndex; // the index of the letter OF EACH WORD on the screen
+    int k; // the index of the word in ArrayList words
     int progressSize; // the size for each progress bar progression
     int progress; // the total number of pixels that the progress bar has progressed
     TimerTask task;
     Timer timer;
-    boolean end;
+    boolean end; // the game ends once timer reaches 1 minute
 
-    class Helper extends TimerTask
-    {
-        public int i = 0;
-        public void run()
-        {
-//            System.out.println("ONE MINUTEEE");
+    // when the timer ends, boolean end is set to true
+    class Helper extends TimerTask {
+        public void run() {
             end = true;
         }
     }
@@ -35,10 +33,11 @@ public class TypingTest extends PApplet {
     public void draw() {
         fill(0, 0, 0);
         textSize(30);
-        text("MUSIC TYPING TEST", 250, 50);
-        int numChar;
-//        text("Type in the artist you want in terminal", 200, 150);
+        text("LYRIC TYPING TEST", 250, 50);
 
+        int numChar; // number of characters on the screen (used to determine progressSize)
+
+        // when the game ends, show user their speed
         if (end) {
             fill(255, 255, 255);
             stroke(255, 255, 255);
@@ -46,71 +45,75 @@ public class TypingTest extends PApplet {
 
             fill(0, 0, 0);
             textSize(30);
-            text("Your speed is: " + (wordIndex + k) + " words per minute", 100, 50);
+            // shows the total number of words user has typed before time ran out
+            text("Your speed is: " + (wordIndex + k - 4) + " words per minute", 100, 50); // subtract 4 because wordIndex starts at 0 and k is 5 ahead
         }
+        // if game has not ended...
+        else {
+            // process what they user types in
+            if (keyPressed) {
+                int x_coord = 50;
 
-        // process what they user types in
-        if (keyPressed) {
-            int x_coord = 50;
+                // when user types ENTER at the beginning of the game, the timer starts and 5 words are generated on the screen
+                // this also generates 5 new words on the screen when a user finishes typing 5 words (wordIndex = 5)
+                if (key == ENTER || key == RETURN || wordIndex == 5) {
+                    if (wordIndex == 0){
+                        System.out.println("Timer start");
+                        timer.schedule(task, 60000 );
+                    }
 
-            if (key == ENTER || key == RETURN || wordIndex == 5) {
-                if (wordIndex == 0){
-                    System.out.println("Timer start");
-                    timer.schedule(task, 60000 );
+                    // reset all progress bar variables to 0
+                    numChar = 0;
+                    progress = 0;
+                    progressSize = 0;
+
+                    fill(255, 255, 255);
+                    stroke(255, 255, 255);
+                    rect(0, 0, 750, 500);
+
+                    fill(0, 0, 0);
+
+                    // get number of characters on the screen & print out the words (5 at a time)
+                    for (int i=0; i<5; i++) {
+                        fill(23,27,42);
+                        numChar += words.get(i+k).length() + 1;
+                        text(words.get(i+k),x_coord, 300);
+                        x_coord += 50 + 15*(words.get(i+k).length());
+                    }
+
+                    // reset each set of 5 words
+                    wordIndex = 0;
+                    k += 5;
+                    keyPressed = false;
+
+                    // calculate how much the progress bar should progress (based on number of characters on the screen)
+                    progressSize = width/numChar;
                 }
 
-                numChar = 0;
-                progress = 0;
-                progressSize = 0;
-
-                fill(255, 255, 255);
-                stroke(255, 255, 255);
-                rect(0, 0, 750, 500);
-
-                fill(0, 0, 0);
-
-                for (int i=0; i<5; i++) {
-                    fill(23,27,42);
-                    numChar += words.get(i+k).length() + 1;
-//                    System.out.println(numChar);
-                    text(words.get(i+k),x_coord, 300);
-                    x_coord += 50 + 15*(words.get(i+k).length());
+                // this runs when you've reached the end a word (a.k.a. letterIndex = -1 (see below)); then you need to type in a space (since that's what comes after a word lol)
+                if (letterIndex == -1 && key == ' ') {
+                    letterIndex += 1;
+                    System.out.print(" ");
+                    progress += progressSize;
+                    wordIndex += 1;
                 }
-                wordIndex = 0;
-                k += 5;
-                keyPressed = false;
+                // checks if the letter you typed matches the current letter based on wordIndex and letterIndex
+                // if it matches, update letterIndex, print out the current letter in terminal, and update progress bar
+                else if (letterIndex != -1 && key == words.get(wordIndex+k-5).charAt(letterIndex)){
+                    letterIndex += 1;
+                    System.out.print(words.get(wordIndex + k - 5).charAt(letterIndex - 1));
+                    progress += progressSize;
+                }
+                // checks if you have reached the end of a word (set letterIndex = -1 to indicate such)
+                if (letterIndex == words.get(wordIndex+k-5).length()){
+                    letterIndex = -1;
+                    keyPressed = false;
+                }
+            }
 
-                progressSize = width/numChar;
-//                System.out.println("Progress bar size: " + progressSize);
-            }
-            if (letterIndex == -1 && key == ' ') {
-                letterIndex += 1;
-                System.out.print(" ");
-                progress += progressSize;
-                wordIndex += 1;
-            }
-            else if (letterIndex != -1 && key == words.get(wordIndex+k-5).charAt(letterIndex)){
-                letterIndex += 1;
-                System.out.print(words.get(wordIndex + k - 5).charAt(letterIndex - 1));
-                progress += progressSize;
-            }
-//            else {
-//                // they didn't type in the right letter
-////                System.out.println("WRONG HAHA LOSER");
-//                fill(255, 0, 0);
-//                rect(0, 490, progress, 10);
-//            }
-            if (letterIndex == words.get(wordIndex+k-5).length()){
-                letterIndex = -1;
-//                wordIndex += 1;
-                keyPressed = false;
-            }
-        }
-
-        // progress bar
-        if (!end) {
+            // progress bar
             fill(0, 255, 0);
-            rect(0, 490, progress, 10);
+            rect(0, 490, progress, 10); // progress determines the width of the bar
         }
     }
 
